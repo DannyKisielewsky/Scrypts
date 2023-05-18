03fe8492502b175e0d6dae34ccbc86dbba5fddf6
DannyKisielewsky <133924278+DannyKisielewsky@users.noreply.github.com>
2023-05-18 02:29:11 +0200

    Stworz.py

--- Stworz.py
+++ Stworz.py
@@ -5,10 +5,20 @@ InventoryList=[]
 AnswerList={'Tak', 'tak', 'Nie', 'nie'}
 pola = ['Dane użytkownika', 'PC', 'iPad SN', 'Phone SN']
 
-dirname = 'C:\\Users\\kisie\\Documents\\Scrypts\\Baza\\Output'
+dirname = 'example C:\\Users\\xx\\xx\\xx\\xx\\xx'
 filename = input(' Wprowadź nazwe dla swojego swojego pliku ')
 filepath = os.path.join(dirname,filename)
 Output = (filepath + '.csv')
+
+while os.path.isfile(Output) == True:
+    filename=input('Plik juz istnieje, wybierz inna nazwe')
+    filepath = os.path.join(dirname,filename)
+    Output = (filepath + '.csv')
+    os.path.isfile(Output)
+    if os.path.isfile(Output) == False:
+        print('Mozna stworzyc plik o nazwie', filename)
+        break
+
 ID=input('Wprowadz dane uzytkownika: ')
 InventoryList.append(ID)
 PC=input('Wprowadz nazwe PC: ')
@@ -26,14 +36,14 @@ def summary():
     print('2. Nazwa PC:         ', InventoryList[1])
     print('3. iPad  SN:         ', InventoryList[2])
     print('4. Phone SN:         ', InventoryList[3])
-
+    print('')
 def SaveOperation():
     with open (Output, mode='w') as plikCSV:
         writer = csv.writer(plikCSV, delimiter=';')
         writer.writerow(InventoryList)
         print('Twoj plik zostal zapisany', Output)
         input('Wcisnij enter by zamknac program')
-        
+summary()        
 check=input('Czy dane są poprawne? ')
 while check == "nie" or check == "Nie":
     print('Ktora kolumne chcesz edytować?')
@@ -70,4 +80,4 @@ while check == "nie" or check == "Nie":
         if check == "tak" or check == "Tak":
             SaveOperation()
             break
-        
+SaveOperation()
